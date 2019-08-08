6a85ddb40e70bb765800f547415c03a10ea823a5
Albert <albert.kulinski@hotmail.com>
2019-08-08 17:12:26 +0200

    intial commit

---
+++ src/main/java/com/akulinski/keyauthservice/KeyauthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.akulinski.keyauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyauthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyauthserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akulinski/keyauthservice/KeyauthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akulinski.keyauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyauthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
